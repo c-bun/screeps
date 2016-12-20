1a65a697de9cdb35e3820b9ceb628517a9368e41
c-bun <colrath@gmail.com>
2016-12-20 01:06:54 -0500

    make harvester functional

--- role.harvester.js
+++ role.harvester.js
@@ -9,40 +9,16 @@ var roleHarvester = {
             }
         }
         else {
-            var primaryTargets = creep.room.find(FIND_STRUCTURES, {
+            var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER
-                                ) && (structure.energy < structure.energyCapacity);
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.energy < structure.energyCapacity;
                     }
             });
-            // this does not work for some reason.
-            var secondaryTargets = creep.room.find(FIND_STRUCTURES , {
-                filter: (structure) => {
-                    return structure.structureType == STRUCTURE_CONTAINER &&
-                    _.sum(structure.store) < structure.storeCapacity;
-                }    
-            });
-            
-            if(primaryTargets.length > 0) {
-                if(creep.transfer(primaryTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(primaryTargets[0]);
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
                 }
-            }
-            else if(secondaryTargets.length > 0) {
-                if(creep.transfer(secondaryTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(secondaryTargets[0]);
-                }
-            }
-            else {
-                creep.say('Nothing to transfer energy to.')
-                var spawn = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_SPAWN)
-                    }
-                });
-                creep.moveTo(spawn[0]);
             }
         }
     }
