095d9d352af61e3bb7ae57bac711ef2c51286c0a
c-bun <colrath@gmail.com>
2016-12-20 01:11:46 -0500

    make functional

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-    
+
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -12,21 +12,21 @@ module.exports.loop = function () {
     }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+//    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+//    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
-    if(harvesters.length < 5) {
+    if(harvesters.length < 3) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         //console.log('Spawning new harvester: ' + newName);
     }
-    if(upgraders.length < 1) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
-        //console.log('Spawning new upgrader: ' + newName);
-    }
-    if(builders.length < 3) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
-        //console.log('Spawning new builder: ' + newName);
-    }
+    // if(upgraders.length < 1) {
+    //     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+    //     //console.log('Spawning new upgrader: ' + newName);
+    // }
+    // if(builders.length < 3) {
+    //     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+    //     //console.log('Spawning new builder: ' + newName);
+    // }
 
     // var tower = Game.getObjectById('TOWER_ID');
     // if(tower) {
