9979c2303be0ad8d12b37d3140c8e1fe53a15a6d
c-bun <colrath@gmail.com>
2016-12-22 11:19:38 -0500

    get rid of source allocation

--- role.harvester.js
+++ role.harvester.js
@@ -3,22 +3,23 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if (creep.carry.energy < creep.carryCapacity) {
-            var toHarvest;
-            if (creep.memory.sourceId) {
-                toHarvest = Game.getObjectById(creep.memory.sourceId);
-            } else {
-                if (!(creep.room.memory.source_coverage)) {
-                  creep.room.memory.source_coverage = {}
-                }
-                var sources = creep.room.find(FIND_SOURCES);
-                for (i = 0; i < sources.length; i++) {
-                    if (!(creep.room.memory.source_coverage[sources[i].id]) || creep.room.memory.source_coverage[sources[i].id] < 2) {
-                        toHarvest = sources[i];
-                        creep.room.memory.source_coverage[sources[i].id]++;
-                        creep.memory.source = sources[i].id;
-                    }
-                }
-            }
+            var toHarvest = creep.room.find(FIND_SOURCES);
+            // TODO This method of allocation works! need to implement with manager
+            // if (creep.memory.sourceId) {
+            //     toHarvest = Game.getObjectById(creep.memory.sourceId);
+            // } else {
+            //     if (!(creep.room.memory.source_coverage)) {
+            //       creep.room.memory.source_coverage = {}
+            //     }
+            //     var sources = creep.room.find(FIND_SOURCES);
+            //     for (i = 0; i < sources.length; i++) {
+            //         if (!(creep.room.memory.source_coverage[sources[i].id]) || creep.room.memory.source_coverage[sources[i].id] < 2) {
+            //             toHarvest = sources[i];
+            //             creep.room.memory.source_coverage[sources[i].id]++;
+            //             creep.memory.source = sources[i].id;
+            //         }
+            //     }
+            // }
             if (creep.harvest(toHarvest) == ERR_NOT_IN_RANGE) {
               console.log(creep.name , " moving to " , toHarvest.id)
                 creep.moveTo(toHarvest);
